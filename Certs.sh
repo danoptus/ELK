79feb0d78dd0ee8ae24a47cef32808579a1051b5
H0lmmes <88737279+H0lmmes@users.noreply.github.com>
2021-08-26 16:17:08 -0300

    Update Certs.sh

--- Certs.sh
+++ Certs.sh
@@ -39,13 +39,13 @@ read datas
   for i in {1..$i}
   do  
   echo "
-  Informe o ip dos hosts: "
+  Informe o ip dos hosts masters: "
   read ip
   ./bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip $ip --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/master-$i.p12 --pass
  
  else
  echo "
- Informe o ip do host: "
+ Informe o ip do host master: "
  read ip
  ./bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip $ip --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/master-1.p12 --pass
  
@@ -55,13 +55,13 @@ read datas
   for i in {1..$i}
   do  
   echo "
-  Informe o ip dos hosts: "
+  Informe o ip dos hosts datas: "
   read ip
   ./bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip $ip --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/data-$i.p12 --pass
  
  else
  echo "
- Informe o ip do host: "
+ Informe o ip do host data: "
  read ip
  ./bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip $ip --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/data-1.p12 --pass
  
