a0d788c111cf62e21b140cb154f83d9f6fc262f0
H0lmmes <88737279+H0lmmes@users.noreply.github.com>
2021-08-26 17:59:28 -0300

    Update Certs.py

--- Certs.py
+++ Certs.py
@@ -30,9 +30,9 @@ os.system("mkdir -p /opt/elastic/elasticsearch-7.5.1/config/certs && /opt/elasti
  
 if masters > 1:
     for i in range(masters):
-    ip =  input("\nInforme o ip dos hosts masters: ")
-     config_certs  = "/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca  /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip {}  --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/master-{}.p12 --pass".format(ip,i)
-     os.system(config_certs)
+      ip =  input("\nInforme o ip dos hosts masters: ")
+      config_certs  = "/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca  /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip {}  --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/master-{}.p12 --pass".format(ip,i)
+      os.system(config_certs)
      
   
 else:
