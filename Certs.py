6e4ad9f768a6ddea1701948eb892b56231befd51
H0lmmes <88737279+H0lmmes@users.noreply.github.com>
2021-08-26 17:04:54 -0300

    Update Certs.py

--- Certs.py
+++ Certs.py
@@ -10,32 +10,32 @@ print ("""
 ++++++++++++++++++++++++++++++++++++++++++
  """)
  
- os.system("sleep 3")
+os.system("sleep 3")
  
 masters = input("\n Informe a quantidade de masters no ambiente: ")
 datas = input( "\nInforme a quantidade de datas no ambiente: ")
  
  
  #Gera CA auto assinada
- print ("""
+print ("""
  ##################################
  Gerando certificado auto-assinado
  #################################
  """)
- os.system("sleep 2")
- os.system("mkdir -p /opt/elastic/elasticsearch-7.5.1/config/certs
- /opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil ca --out /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --pass")
+os.system("sleep 2")
+os.system("mkdir -p /opt/elastic/elasticsearch-7.5.1/config/certs
+/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil ca --out /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --pass")
  
  
  #Gerando certificado para os datas e masters
  
- if masters > 1:
+if masters > 1:
     for i in range(masters):
      ip =  input("\nInforme o ip dos hosts masters: "
      os.system("/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip"+ip+" --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/master-"+i".p12 --pass")
      
   
- else
+else
  ip = input("\nInforme o ip do host master: ")
  os.system("/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip"+ip+" --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/master-1.p12 --pass")
  
