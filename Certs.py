d724b1a84453a73c2bbfe5e7cf6cb643500fd4e1
H0lmmes <88737279+H0lmmes@users.noreply.github.com>
2021-08-26 18:18:30 -0300

    Update Certs.py

--- Certs.py
+++ Certs.py
@@ -38,32 +38,44 @@ if masters > 1:
     for i in range(masters):
       i = i+1
       ip =  input("\nInforme o ip do master-{}: ".format(i))
+      os.system("clear")
       config_certs  = "/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca  /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip {}  --out /opt/elastic/elasticsearch-7.5.1/config/certs/master-{}.p12 --pass".format(ip,i)
       os.system(config_certs)
+      os.system("clear")
      
   
 else:
- ip = input("\nInforme o ip do host master: ")
+ ip = input("\nInforme o ip do master-1: ")
  config_certs = "/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca  /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip {} --out /opt/elastic/elasticsearch-7.5.1/config/certs/master-1.p12 --pass".format(ip)
  os.system(config_certs)
+ os.system("clear")
  
-'''
- if $datas > 1
- then
-  for i in {1..$i}
-  do  
-  echo "
-  Informe o ip dos hosts datas: "
-  read ip
- /opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip $ip --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/data-$i.p12 --pass
-  done
+ 
+ 
+ 
+os.system("clear")
+print("""
+ #########################################
+ Criando certificados para o server data
+ #########################################
+ """)
+ 
+if datas > 1:
+    for i in range(datas):
+      i = i+1
+      ip =  input("\nInforme o ip do master-{}: ".format(i))
+      os.system("clear")
+      config_certs  = "/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca  /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip {}  --out /opt/elastic/elasticsearch-7.5.1/config/certs/data-{}.p12 --pass".format(ip,i)
+      os.system(config_certs)
+      os.system("clear")
+     
   
- else
- echo "
- Informe o ip do host data: "
- read ip
- /opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip $ip --out /opt/elastic/elasticsearch-7.5.1/config/certs/certs/data-1.p12 --pass
- fi
- '''
+else:
+ ip = input("\nInforme o ip do master-1: ")
+ config_certs = "/opt/elastic/elasticsearch-7.5.1/bin/elasticsearch-certutil cert --ca  /opt/elastic/elasticsearch-7.5.1/config/certs/ca.p12 --ca-pass --ip {} --out /opt/elastic/elasticsearch-7.5.1/config/certs/data-1.p12 --pass".format(ip)
+ os.system(config_certs)
+ os.system("clear")
  
+ 
+
  
